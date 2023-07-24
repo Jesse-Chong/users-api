2e8a08f3c16c5783f69ebba9a48345d329a859c0
Jesse Chong <jessechong@pursuit.org>
2023-07-24 10:18:18 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -7,22 +7,20 @@ const bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 app.get('/users/:id', (req, res) => {
-    const championId = parseInt(req.params.id);
-    const champion = users.find(user => user.id === championId);
-  
-    if (champion) {
-      champion.lore = champion.lore.replace(/\n/g, '<br>');
-      champion.attributes = champion.attributes.replace(/\n/g, '<br>');
-      Object.values(champion.skills).forEach(skill => {
-        skill.skillDescription = skill.skillDescription.replace(/\n/g, '<br>');
-      });
-  
-      res.json(champion);
-    } else {
-      res.status(404).json({ message: 'Champion not found' });
-    }
-  });
+  const championId = parseInt(req.params.id);
+  const champion = users.find(user => user.id === championId);
+
+  if (champion) {
+    res.json(champion);
+  } else {
+    res.status(404).json({ message: 'Champion not found' });
+  }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
